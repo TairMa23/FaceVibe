1c854d0a77520ec81f251e30fd94598e651d5cfc
reut <r9822501@gmail.com>
2024-08-08 16:50:43 +0300

    add bs"d

--- client/src/components/FooterBar/FooterBar.jsx
+++ client/src/components/FooterBar/FooterBar.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 import { FaFacebookF } from "react-icons/fa";
-// import { FaFacebookF, FaTwitter } from "react-icons/fa";
-// import { AiFillInstagram } from "react-icons/ai";
 import { Link } from "react-router-dom";
 
 const FooterBar = () => {
   return (
     <div className="bg-[#E7E4F8] py-2">
+     <p className="text-white text-sm"> בס"ד</p>
     </div>
   );
 };
