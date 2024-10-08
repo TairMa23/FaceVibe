ae05a31aef9c1229f93707c8839e317fb61a3997
Tair <tairmazuz15@gmail.com>
2024-10-09 00:18:28 +0300

    update to real IP

--- client/src/store/axiosConfig.ts
+++ client/src/store/axiosConfig.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const baseURL = "http://localhost:8080";
+export const baseURL = "http://185.159.109.246:8080";
 
 const axiosInstance = axios.create({
   baseURL,
